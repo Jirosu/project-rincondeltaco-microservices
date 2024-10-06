cec4078f9bad7b4baafed2f49d1bb04a0f984ad6
jimmy.nh47@gmail.com <jimmy120921@gmail.com>
2024-10-05 23:28:18 -0500

    Initial commit

---
+++ vouchers-service/src/main/java/com/rincondeltaco/vouchers_service/VouchersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rincondeltaco.vouchers_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouchersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouchersServiceApplication.class, args);
+	}
+
+}
---
+++ vouchers-service/src/test/java/com/rincondeltaco/vouchers_service/VouchersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rincondeltaco.vouchers_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouchersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
